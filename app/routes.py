03e5d487ee52157cdea2f71dfad2b08e0ebb37f9
Maksym Izmailov <maksym.izmailov.lv@gmail.com>
2020-12-20 21:27:28 +0200

    variant db.sessions.add(post)

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,20 @@
 from flask_restful import Resource
+from flask import request
 from . import api
 from . import models
 
 
 class PostListApi(Resource):
+
+    def post(self):
+        print(request.json)
+        title = request.json['title']
+        body = request.json['body']
+        date = request.json['date']
+        post = models.Post(title, body, date)
+        db.sessions.add(post)
+        db.sessions.commit()
+        return {'title': title, 'body': body, 'date': date}, 201
 
     def get(self):
         return {"message": "Hello"}
