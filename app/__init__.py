2971f3f5908ccc64f71513c8152838a425d13b8d
Maksym Izmailov <maksym.izmailov.lv@gmail.com>
2020-12-20 19:58:45 +0200

    naming convention

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,21 @@
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from flask_migrate import Migrate
 import config
 
+convention = {
+  "ix": "ix_%(column_0_label)s",
+  "uq": "uq_%(table_name)s_%(column_0_name)s",
+  "ck": "ck_%(table_name)s_%(constraint_name)s",
+  "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+  "pk": "pk_%(table_name)s"
+}
 
 app = Flask(__name__)
 app.config.from_object(config.Config)
-db = SQLAlchemy(app)
+db = SQLAlchemy(app, metadata=MetaData(naming_convention=convention))
 migrate = Migrate(app, db)
 api = Api(app)
 
