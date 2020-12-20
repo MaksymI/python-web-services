a6381100871af558503cf8fb32b0204b40153052
Maksym Izmailov <maksym.izmailov.lv@gmail.com>
2020-12-20 22:03:25 +0200

    post using marshmallow

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,22 @@
 from flask_restful import Resource
 from flask import request
-from . import api
-from . import models
+from marshmallow.exceptions import ValidationError
+from . import api, db, schemas
+
+post_schema = schemas.PostSchema()
 
 
 class PostListApi(Resource):
 
     def post(self):
-        print(request.json)
-        title = request.json['title']
-        body = request.json['body']
-        date = request.json['date']
-        post = models.Post(title, body, date)
-        db.sessions.add(post)
-        db.sessions.commit()
-        return {'title': title, 'body': body, 'date': date}, 201
+        try:
+            post = post_schema.load(request.json, session=db.session)
+        except ValidationError as e:
+            return {"message": str(e)}, 400
+
+        db.session.add(post)
+        db.session.commit()
+        return post_schema.dump(post), 201
 
     def get(self):
         return {"message": "Hello"}
