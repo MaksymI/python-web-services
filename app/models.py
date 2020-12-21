0f595fe90512c215cb62be75be4749f84f4cfbc6
Maksym Izmailov <maksym.izmailov.lv@gmail.com>
2020-12-21 20:07:29 +0200

    User model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,29 @@
 import uuid
 
+from werkzeug.security import generate_password_hash
+
 from . import db
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(50), unique=True)
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(94))
+    is_staff = db.Column(db.Boolean, default=False)
+    uuid = db.Column(db.String(36), unique=True)
+
+    def __init__(self, login, email, password):
+        self.login = login
+        self.email = email
+        self.password = generate_password_hash(password)
+        self.is_staff = False
+        self.uuid = str(uuid.uuid4())
+
+    def __repr__(self):
+        return f"User('{self.login}', '{self.uuid}')"
+
 
 
 class Post(db.Model):
