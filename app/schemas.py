ef876c98e8a7d727418df46d06c9500186f4fe00
Maksym Izmailov <maksym.izmailov.lv@gmail.com>
2020-12-25 20:55:46 +0200

    nested schema for author

--- app/schemas.py
+++ app/schemas.py
@@ -1,17 +1,20 @@
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow import fields
 from . import models
-
-
-class PostSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = models.Post
-        exclude = 'id',
-        # так как будем создавать новых юзеров
-        load_instance = True
 
 
 class UserSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = models.User
         exclude = 'id',
+        load_instance = True
+
+
+class PostSchema(SQLAlchemyAutoSchema):
+    author = fields.Nested(UserSchema)
+
+    class Meta:
+        model = models.Post
+        exclude = 'id',
+        # так как будем создавать новых юзеров
         load_instance = True
